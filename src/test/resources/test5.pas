7c5f917158fad19f569fa1482df5a4d5266141dd
Sébastien Le Callonnec <sebastien@weblogism.com>
2016-10-03 20:02:59 +0100

    Implement while and for.

--- src/test/resources/test5.pas
+++ src/test/resources/test5.pas
@@ -5,4 +5,9 @@ begin
      do_something;
      x := x + 1;
    until x = 10;
+   while X > 0 do
+     begin
+       x := x - 1;
+       do_something_else;
+     end;
 end.
---
+++ src/test/resources/test6.pas
@@ -0,0 +1,11 @@
+program test6;
+begin
+   for I:=100 downto 1 do
+      begin
+         writeln('value of a: ', I);
+      end;
+   for I:=1 to 100 do
+      begin
+        writeln('value of a: ', I);
+      end;
+end.
